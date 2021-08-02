791b4c1eafefdba1d0d48d6b14d9e3a49d61c644
Vong Nyuk Soon <vongnyuksoon2000@gmail.com>
2021-08-03 01:30:44 +0800

    t

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,12 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CatsModule } from './cats/cats.module';
 
 describe('AppController', () => {
   let appController: AppController;
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
+      imports: [CatsModule],
       controllers: [AppController],
       providers: [AppService],
     }).compile();
